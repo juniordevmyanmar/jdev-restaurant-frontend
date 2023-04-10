fdf18aa08d1e207d4ea270a2eb962a2d24c09fa7
AungKhantMin21 <aungkhantmin212002@gmail.com>
2023-04-10 12:01:12 +0630

    Add schema-based form validation

--- src/features/login/ui/LoginForm.tsx
+++ src/features/login/ui/LoginForm.tsx
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useForm, SubmitHandler } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import AlertBox from "./AlertBox";
 
-type Inputs =  {
-    email: string,
-    password: string
-}
+const schema = yup.object({
+    email: yup.string()
+                .email("Invalid email address")
+                .required("Please enter your email"),
+    password: yup.string()
+                    .required("Please enter your password")
+                    .min(6,"must be at least 6 characters long")
+}).required();
+
+type Inputs =  yup.InferType<typeof schema>;
 
 interface User{
     id:number,
@@ -16,7 +24,9 @@ interface User{
 }
 
 const LoginForm = () => {
-    const {register, handleSubmit,formState: {errors}} = useForm<Inputs>();
+    const {register, handleSubmit,formState: {errors}} = useForm<Inputs>({
+        resolver: yupResolver(schema)
+    });
     const [isCorrect, setIsCorrect] = useState<string>("uncheck");
     
     const getUser = (email:string,password:string) => {
@@ -50,17 +60,17 @@ const LoginForm = () => {
                 <AlertBox handleCloseBtn={handleCloseBtn}/>
             )}
             <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center justify-center">
-                <input type="email" id="email" className={`w-full sm:w-[476px] block rounded-md p-2.5 gray-900 text-gray-900  shadow-sm ring-1 ring-inset ring-[#E1688C]  focus:ring-2 focus:ring-inset focus:ring-[#E1688C] sm:text-sm sm:leading-6 pl-[10px] ${errors.email ? "border border-red-500 placeholder:text-red-400": "border-0 placeholder:text-gray-400"}`} placeholder="Enter Email"  {...register("email", {required:"Please enter your email"})}/>
+                <input type="email" id="email" className={`w-full sm:w-[476px] block rounded-md p-2.5 gray-900 text-gray-900  shadow-sm ring-1 ring-inset ring-[#E1688C]  focus:ring-2 focus:ring-inset focus:ring-[#E1688C] sm:text-sm sm:leading-6 pl-[10px] ${errors.email ? "border border-red-500 placeholder:text-red-400": "border-0 placeholder:text-gray-400"}`} placeholder="Enter Email"  {...register("email")}/>
                 {errors.email && (
                     <div className="w-full sm:w-[476px] text-left">
-                        <span className="text-red-600">{errors.email.message}</span>
+                        <span className="text-red-600">{errors.email?.message}</span>
                     </div>
                 )}
 
-                <input type="password" id="password" className={`w-full sm:w-[476px] mt-8 md:mt-10 block rounded-md p-2.5 gray-900 text-gray-900  shadow-sm ring-1 ring-inset ring-[#E1688C] focus:ring-2 focus:ring-inset focus:ring-[#E1688C]  sm:text-sm sm:leading-6 pl-[10px] ${errors.password ? "border border-red-500 placeholder:text-red-400": "border-0 placeholder:text-gray-400"}`} placeholder="Enter Password" {...register("password",{required:"Please enter your password"})}/>
+                <input type="password" id="password" className={`w-full sm:w-[476px] mt-8 md:mt-10 block rounded-md p-2.5 gray-900 text-gray-900  shadow-sm ring-1 ring-inset ring-[#E1688C] focus:ring-2 focus:ring-inset focus:ring-[#E1688C]  sm:text-sm sm:leading-6 pl-[10px] ${errors.password ? "border border-red-500 placeholder:text-red-400": "border-0 placeholder:text-gray-400"}`} placeholder="Enter Password" {...register("password")}/>
                 {errors.password && (
                     <div className="w-full sm:w-[476px] text-left">
-                        <span className="text-red-600">{errors.password.message}</span>
+                        <span className="text-red-600">{errors.password?.message}</span>
                     </div>
                 )}
 
