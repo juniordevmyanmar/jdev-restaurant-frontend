72fc26059aa9256bf25b95345b8fd33d6ee445d0
San San Thi <sansanthi1996@gmail.com>
2023-04-23 19:34:40 +0630

    Menu item list UI (#9)
    * menu item show ui
    
    * added typescript type props
    
    * change image url in App
    
    ---------
    
    Co-authored-by: Aung Khant Min <58725895+AungKhantMin21@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
+          
         </Routes>
       </BrowserRouter>
       </div>
