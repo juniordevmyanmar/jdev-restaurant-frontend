e3b913b39a4afe88d40595c7387f2b898d11ad82
Aung Khant Min <58725895+AungKhantMin21@users.noreply.github.com>
2023-05-11 19:53:06 +0630

    menu-list api integration (#13)

--- src/features/menuList/ui/MenuList.tsx
+++ src/features/menuList/ui/MenuList.tsx
@@ -1,10 +1,11 @@
 import "./index.css";
 
 type MenuListProps = {
-  menus: { id: number; menu_name: string; menu_image: string }[];
+  menus: { id: number; name: string; coverImage: string }[];
 };
 
 const MenuList = ({ menus }: MenuListProps) => {
+  
   return (
     <div className="menulist-container text-white pt-10 pl-10">
       <h2 className="mb-6 text-xl">Available Menu</h2>
@@ -15,12 +16,12 @@ const MenuList = ({ menus }: MenuListProps) => {
               <div className="menu-box" key={menu.id}>
                 <div className="w-[200px] h-[200px] sm:h-[300px] mb-3">
                   <img
-                    src={menu.menu_image}
+                    src={menu.coverImage}
                     alt=""
                     className="w-full h-full object-cover rounded-lg menu-box--img"
                   />
                 </div>
-                <h3 className="text-sm">{menu.menu_name}</h3>
+                <h3 className="text-sm">{menu.name}</h3>
                 <button className="text-xs rounded-full py-1 mt-4 px-8 btn-order">
                   Order
                 </button>
