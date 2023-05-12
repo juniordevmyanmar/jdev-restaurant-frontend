3045b25522d833416c6b0f85048c64e49305a811
Kaung Htet Naing <11kaungkaung99@gmail.com>
2023-05-12 21:56:20 +0630

    add ResturantCatList to HomePage

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,7 @@
-export { default as LoginScreen } from './LoginPage';
-export { default as DealsScreen } from './DealsPage';
-export { default as FoodCategoryScreen } from './FoodCategoryoPage';
-export { default as HomScreen } from './HomePage';
-export { default as ResturantsScreen } from './ResturantsPage'
+export { default as LoginPage } from './LoginPage';
+export { default as RegisterPage } from './RegisterPage';
+export { default as DealsPage } from './DealsPage';
+export { default as FoodCategoryPage } from './FoodCategoryoPage';
+export { default as HomePage } from './HomePage';
+export { default as ResturantsPage } from './ResturantsPage'
+export { default as RestaurantDetailPage } from './RestaurantDetailPage'
